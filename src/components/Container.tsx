3565f56aafb7e165cd512cd2306e64ae66d6b844
Wendy Beck <webeck@gmail.com>
2021-07-16 18:06:16 -0700

    stopping for now

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,16 +2,24 @@ import React, {FunctionComponent} from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
-
+import styled from "styled-components";
+import {color} from "styled-system";
 
 type ContainerProps = {
 	title: string;
 };
 
+const Title = styled.h1`
+${color}
+font-size: 1.5em;
+text-align: center;
+`;
+
 const Container: FunctionComponent<ContainerProps> = ({title}) => {
 	return (
 		<div>
 			<Header message={title} />
+			<Title color="darkMagenta">You call this a title?</Title>
 			<Main />
 			<Footer message="I am the footer" />
 		</div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,20 @@
 import React, {FunctionComponent} from "react";
+import Container from "./Container";
+import styled from "styled-components";
+import {color} from "styled-system";
 
 type MainProps = {
 }
-
+const MainContainer = styled(Container)`
+${color}
+`;
 
 const Main: FunctionComponent<MainProps> = () => {
 
 	return (
 		<div>
-Main!
+			<MainContainer color="buttonBlue">Main!</MainContainer>
+
 		</div>
 	);
 };
